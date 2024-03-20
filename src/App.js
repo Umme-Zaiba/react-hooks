2a8b22195f40c7f73838bdead4f449dfb26df1b7
Umme Zaiba <83930541+Umme-Zaiba@users.noreply.github.com>
2024-03-20 14:18:00 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Color from './components/Color'
 
+import Props2 from './components/props/Props2';
+
 function App() {
 
   return (
     <div className="App">
+
+      <Color/>   
+
       <Color/>      
+
     </div>
   );
 }
