9ff143ebb7600153d62f16f339b16f771fd68c41
Umme Zaiba <ummezaiba24j@gmail.com>
2024-03-20 14:53:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import Color from './components/Color'
 
 import Props2 from './components/props/Props2';
+import Hooks from './components/useState/Hooks';
 
 function App() {
-
   return (
     <div className="App">
-
-      <Color/>   
-
       <Color/>      
-
+      <Props2/>
+      <Hooks/>
     </div>
   );
 }
